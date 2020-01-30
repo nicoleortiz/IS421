a322a11b961784b264ae860e9f533830065465de
Nicole Ortiz <no59@njit.edu>
2020-01-30 18:27:02 -0500

    Initial Code

---
+++ Calculator/basiccalculator/calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace basiccalculator
+{
+    public class Calculator
+    {
+        static public int Add(int a, int b)
+        {
+            var c = a + b;
+
+            return c;
+        }
+        static public int Subtract(int a, int b)
+        {
+            var c = a - b;
+
+            return c;
+        }
+
+    }
+}
---
+++ Calculator/basiccalculatorTests/CalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using basiccalculator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace basiccalculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            var c = Calculator.Add(1, 2);
+            Assert.AreEqual(3, c);
+
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            var c = Calculator.Subtract(1, 2);
+            Assert.AreEqual(-1, c);
+        }
+    }
+}
